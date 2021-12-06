4937eb0b90665e5446b5e3f498f307b749612703
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2021-12-06 15:33:23 +0900

    feature/use common pointcloud container (#8)
    * add arg
    
    * improve readability

--- aip_x1_launch/launch/pointcloud_preprocessor.launch.py
+++ aip_x1_launch/launch/pointcloud_preprocessor.launch.py
@@ -88,32 +88,35 @@ def launch_setup(context, *args, **kwargs):
 
     # set container to run all required components in the same process
     container = ComposableNodeContainer(
-        name="pointcloud_preprocessor_container",
-        namespace="pointcloud_preprocessor",
+        name=LaunchConfiguration("container_name"),
+        namespace="",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
         composable_node_descriptions=[],
+        condition=UnlessCondition(LaunchConfiguration("use_pointcloud_container")),
         output="screen",
+    )
+
+    target_container = (
+        container
+        if UnlessCondition(LaunchConfiguration("use_pointcloud_container")).evaluate(context)
+        else LaunchConfiguration("container_name")
     )
 
     # load concat or passthrough filter
     concat_loader = LoadComposableNodes(
         composable_node_descriptions=[concat_component],
-        target_container=container,
-        condition=launch.conditions.IfCondition(LaunchConfiguration("use_concat_filter")),
+        target_container=target_container,
+        condition=IfCondition(LaunchConfiguration("use_concat_filter")),
     )
 
     passthrough_loader = LoadComposableNodes(
         composable_node_descriptions=[passthrough_component],
-        target_container=container,
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration("use_concat_filter")),
+        target_container=target_container,
+        condition=UnlessCondition(LaunchConfiguration("use_concat_filter")),
     )
 
-    return [
-        container,
-        concat_loader,
-        passthrough_loader,
-    ]
+    return [container, concat_loader, passthrough_loader]
 
 
 def generate_launch_description():
@@ -125,10 +128,11 @@ def generate_launch_description():
 
     add_launch_arg("base_frame", "base_link")
     add_launch_arg("use_concat_filter", "true")
-    add_launch_arg("use_radius_search", "true")
     add_launch_arg("vehicle_param_file")
     add_launch_arg("use_multithread", "False")
     add_launch_arg("use_intra_process", "False")
+    add_launch_arg("use_pointcloud_container", "False")
+    add_launch_arg("container_name", "pointcloud_preprocessor_container")
 
     set_container_executable = SetLaunchConfiguration(
         "container_executable",
