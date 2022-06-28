4da4451a131cc659b0aec4a6fc7aae955e8f283c
kminoda <44218668+kminoda@users.noreply.github.com>
2022-06-28 15:22:50 +0900

    feat(distortion_corrector): use gyroscope for correcting LiDAR distortion (#67)
    * feat: use gyroscope in distortion corrector
    
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>
    
    * edit
    
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- aip_x2_launch/launch/pandar_node_container.launch.py
+++ aip_x2_launch/launch/pandar_node_container.launch.py
@@ -174,6 +174,8 @@ def launch_setup(context, *args, **kwargs):
         plugin="pointcloud_preprocessor::DistortionCorrectorComponent",
         name="distortion_corrector_node",
         remappings=[
+            ("~/input/twist", "/sensing/vehicle_velocity_converter/twist_with_covariance"),
+            ("~/input/imu", "/sensing/imu/imu_data"),
             ("~/input/velocity_report", "/vehicle/status/velocity_status"),
             ("~/input/pointcloud", "mirror_cropped/pointcloud_ex"),
             ("~/output/pointcloud", "rectified/pointcloud_ex"),
