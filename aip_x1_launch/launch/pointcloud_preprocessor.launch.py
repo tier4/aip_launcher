bd70ade9bdc0790d32689d7f35241ebab90a0cd6
Shunsuke Miura <shunsuke.miura@tier4.jp>
2022-11-07 18:27:36 +0900

    Change concatenate input topic
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>

--- aip_x1_launch/launch/pointcloud_preprocessor.launch.py
+++ aip_x1_launch/launch/pointcloud_preprocessor.launch.py
@@ -36,9 +36,7 @@ def launch_setup(context, *args, **kwargs):
             {
                 "input_topics": [
                     "/sensing/lidar/top/outlier_filtered/pointcloud",
-                    "/sensing/lidar/front_left/min_range_cropped/pointcloud",
-                    "/sensing/lidar/front_right/min_range_cropped/pointcloud",
-                    "/sensing/lidar/front_center/min_range_cropped/pointcloud",
+                    "/sensing/lidar/front_center/pointcloud_raw",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
                 "timeout_sec": 1.0,
