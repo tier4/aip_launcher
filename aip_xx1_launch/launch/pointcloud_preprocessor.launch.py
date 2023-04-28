4a50e4ff44da0454af3a7817815fef0c824200eb
Yoshi Ri <yoshiyoshidetteiu@gmail.com>
2023-04-28 20:13:03 +0900

    feat: change pointcloud concatenation timeout setting for each lidar sensors in xx1 laucher (#134)
    * change lidar sync timeout settings
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    * ci(pre-commit): autofix
    
    * fix input offset size to fit with input topic num
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    * ci(pre-commit): autofix
    
    * use left/right lidar
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    * ci(pre-commit): autofix
    
    ---------
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- aip_xx1_launch/launch/pointcloud_preprocessor.launch.py
+++ aip_xx1_launch/launch/pointcloud_preprocessor.launch.py
@@ -36,11 +36,18 @@ def launch_setup(context, *args, **kwargs):
             {
                 "input_topics": [
                     "/sensing/lidar/top/outlier_filtered/pointcloud",
-                    # "/sensing/lidar/left/outlier_filtered/pointcloud",
-                    # "/sensing/lidar/right/outlier_filtered/pointcloud",
+                    "/sensing/lidar/left/outlier_filtered/pointcloud",
+                    "/sensing/lidar/right/outlier_filtered/pointcloud",
                     "/sensing/lidar/rear/outlier_filtered/pointcloud",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
+                "input_offset": [
+                    0.055,
+                    0.025,
+                    0.025,
+                    0.025,
+                ],  # each sensor will wait 20, 50, 50, 50ms
+                "timeout_sec": 0.075,  # set shorter than 100ms
             }
         ],
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
