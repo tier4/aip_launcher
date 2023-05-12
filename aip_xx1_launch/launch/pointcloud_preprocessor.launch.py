27111dcb60a63d0c385e0672f9571879b3583703
Tomoya Kimura <tomoya.kimura@tier4.jp>
2023-05-12 20:30:34 +0900

    feat: change concatenated settings (#136)
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- aip_xx1_launch/launch/pointcloud_preprocessor.launch.py
+++ aip_xx1_launch/launch/pointcloud_preprocessor.launch.py
@@ -47,7 +47,7 @@ def launch_setup(context, *args, **kwargs):
                     0.025,
                     0.025,
                 ],  # each sensor will wait 20, 50, 50, 50ms
-                "timeout_sec": 0.075,  # set shorter than 100ms
+                "timeout_sec": 0.095,  # set shorter than 100ms
             }
         ],
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
