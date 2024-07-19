8c5582decbc1d0bf36b1ac48d2e44ef5c8f254a9
1222-takeshi <m.takeshi1995@gmail.com>
2024-07-19 14:09:19 +0900

    feat: nebula_hw_monitor is not started except when simulation is running
    Signed-off-by: 1222-takeshi <m.takeshi1995@gmail.com>

--- common_sensor_launch/launch/nebula_node_container.launch.py
+++ common_sensor_launch/launch/nebula_node_container.launch.py
@@ -152,6 +152,7 @@ def launch_setup(context, *args, **kwargs):
                     ),
                 },
             ],
+            condition=IfCondition(LaunchConfiguration("launch_driver")),
             extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
         )
     )
