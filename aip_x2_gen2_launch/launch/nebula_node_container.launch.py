b8bf288c87c11a2c54c11a8dcdf74590bddd5cd7
Tomohito ANDO <tomohito.ando@tier4.jp>
2024-10-04 10:57:04 +0900

    fix pre-commit errors
    Signed-off-by: Tomohito ANDO <tomohito.ando@tier4.jp>

--- aip_x2_gen2_launch/launch/nebula_node_container.launch.py
+++ aip_x2_gen2_launch/launch/nebula_node_container.launch.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
@@ -83,7 +81,6 @@ def launch_setup(context, *args, **kwargs):
     # Model and make
     sensor_model = LaunchConfiguration("sensor_model").perform(context)
     sensor_make, sensor_extension = get_lidar_make(sensor_model)
-    nebula_decoders_share_dir = get_package_share_directory("nebula_decoders")
 
     glog_component = ComposableNode(
         package="glog_component",
