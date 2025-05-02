de3f5ffec09d8763d7d8d0b2e1b264a5ca9b529a
vividf <yihsiang.fang@tier4.jp>
2025-05-02 16:18:35 +0900

    chore: add paramter for diag message
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- aip_common_sensor_launch/launch/nebula_node_container.launch.py
+++ aip_common_sensor_launch/launch/nebula_node_container.launch.py
@@ -235,6 +235,7 @@ def make_preprocessor_nodes(context):
 
     cropbox_parameters = create_parameter_dict("input_frame", "output_frame")
     cropbox_parameters["negative"] = True
+    cropbox_parameters["processing_time_threshold"] = 0.01
 
     vehicle_info = get_vehicle_info(context)
     cropbox_parameters["min_x"] = vehicle_info["min_longitudinal_offset"]
