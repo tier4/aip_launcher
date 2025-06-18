2f67de4bf337e9352488f7e546887696dde251c3
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2025-06-18 20:12:58 +0900

    feat(aip_xx1_gen2): use rear lidar in gen2 vehicle except for 8 (#468)
    * add rear lidar entry
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * support both of argument and environment vars
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * ci(pre-commit): autofix
    
    * need to to compensate motion
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * publish rear lidar tf_static
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * update rear lidar sync angle
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * fix spell check
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- aip_xx1_gen2_launch/launch/pointcloud_preprocessor.launch.py
+++ aip_xx1_gen2_launch/launch/pointcloud_preprocessor.launch.py
@@ -21,19 +21,40 @@ from launch.actions import OpaqueFunction
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
+from launch.substitutions import EnvironmentVariable
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 from launch_ros.parameter_descriptions import ParameterFile
+import yaml
+
+
+def erase_rear_lidar_entry_depending_on_vehicle_id(config: dict, vehicle_id: str) -> dict:
+    # Only NO. 8 vehicle does not have a rear lidar, so we erase the rear lidar entry.
+    if vehicle_id != "8":
+        return config
+
+    # Acquire the index of the rear lidar entry
+    rear_lidar_index = config["input_topics"].index("/sensing/lidar/rear/pointcloud_before_sync")
+
+    # Remove all items related to the rear lidar
+    config["input_topics"].pop(rear_lidar_index)
+    config["matching_strategy"]["lidar_timestamp_offsets"].pop(rear_lidar_index)
+    config["matching_strategy"]["lidar_timestamp_noise_window"].pop(rear_lidar_index)
+
+    return config
 
 
 def launch_setup(context, *args, **kwargs):
-    # concatenate node parameters
-    concatenate_and_time_sync_node_param = ParameterFile(
-        param_file=LaunchConfiguration("concatenate_and_time_sync_node_param_path").perform(
-            context
-        ),
-        allow_substs=True,
+    # Load concatenate node parameters as YAML
+    with open(
+        LaunchConfiguration("concatenate_and_time_sync_node_param_path").perform(context), "r"
+    ) as f:
+        concatenate_and_time_sync_node_param = yaml.safe_load(f)["/**"]["ros__parameters"]
+
+    # Remove the rear lidar entry from the parameter file if the vehicle does not have a rear lidar
+    concatenate_and_time_sync_node_param = erase_rear_lidar_entry_depending_on_vehicle_id(
+        concatenate_and_time_sync_node_param, LaunchConfiguration("vehicle_id").perform(context)
     )
 
     # set concat filter as a component
@@ -71,7 +92,10 @@ def generate_launch_description():
     add_launch_arg("use_intra_process", "False")
     add_launch_arg("pointcloud_container_name", "pointcloud_container")
     add_launch_arg("individual_container_name", "concatenate_container")
-
+    add_launch_arg(
+        "vehicle_id",
+        default_value=EnvironmentVariable("VEHICLE_ID", default_value="default"),
+    )
     add_launch_arg(
         "concatenate_and_time_sync_node_param_path",
         os.path.join(
