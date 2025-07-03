9021bd88967a055db144f3da95eff332b5758578
Max SCHMELLER <max.schmeller@tier4.jp>
2025-07-03 18:53:45 +0900

    feat(x2gen2): nebula Agnocast support (#478)

--- aip_x2_gen2_launch/launch/nebula_node_container.launch.py
+++ aip_x2_gen2_launch/launch/nebula_node_container.launch.py
@@ -26,6 +26,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.actions import Node
+from launch_ros.actions import Node
 from launch_ros.descriptions import ComposableNode
 from launch_ros.parameter_descriptions import ParameterFile
 from launch_ros.substitutions import FindPackageShare
