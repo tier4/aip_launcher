d863f735a5a528955bc142f7a739df408fb1f89a
kimurariku <riku.kimura@tier4.jp>
2025-07-25 20:11:07 +0900

    chore: change path to aip_common_sensor
    Signed-off-by: kimurariku <riku.kimura@tier4.jp>

--- aip_common_sensor_launch/launch/nebula_node_container.launch.py
+++ aip_common_sensor_launch/launch/nebula_node_container.launch.py
@@ -402,7 +402,7 @@ def generate_launch_description():
     add_launch_arg(
         "nebula_common_config_file",
         [
-            FindPackageShare("aip_x1_launch"),
+            FindPackageShare("aip_common_sensor_launch"),
             "/config/nebula_hesai_common.param.yaml",
         ],
         description="file containing parameters common to all Nebula instances",
