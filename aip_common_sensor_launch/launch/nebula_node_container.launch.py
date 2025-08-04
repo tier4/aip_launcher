7fb659b13ae276e0f05065ad136abd9ec8ae6f39
Ericpotato <zhanhong.yan@tier4.jp>
2025-08-04 19:23:39 +0900

    remove hires change
    Signed-off-by: Ericpotato <zhanhong.yan@tier4.jp>

--- aip_common_sensor_launch/launch/nebula_node_container.launch.py
+++ aip_common_sensor_launch/launch/nebula_node_container.launch.py
@@ -141,7 +141,6 @@ def make_nebula_nodes(context):
                         "udp_only",
                         "hires_mode",
                         "diag_span",
-                        "hires_mode",
                         "diagnostics.packet_loss.error_threshold",
                     ),
                 },
@@ -443,7 +442,6 @@ def generate_launch_description():
     add_launch_arg("advanced_diagnostics", "false")
     add_launch_arg("use_multithread", "False", "use multithread")
     add_launch_arg("use_intra_process", "False", "use ROS 2 component container communication")
-    add_launch_arg("hires_mode", "false")
     add_launch_arg(
         "diagnostics.packet_loss.error_threshold",
         "10",
