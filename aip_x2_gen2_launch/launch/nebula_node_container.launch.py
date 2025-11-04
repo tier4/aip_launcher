21662d5bcb984f94b73f6c75d2c66af710eaf8f3
Max Schmeller <6088931+mojomex@users.noreply.github.com>
2025-11-04 18:07:48 +0900

    chore(lidar): disable calibration downloads from LiDAR sensors (backport #621) (#628)
    chore(lidar): disable calibration downloads from LiDAR sensors
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- aip_x2_gen2_launch/launch/nebula_node_container.launch.py
+++ aip_x2_gen2_launch/launch/nebula_node_container.launch.py
@@ -115,6 +115,7 @@ def launch_setup(context, *args, **kwargs):
                     "ptp_lock_threshold",
                     "diag_span",
                     "calibration_file",
+                    "calibration_download_enabled",
                     "launch_hw",
                     "udp_only",
                     "point_filters.downsample_mask.path",
@@ -353,6 +354,7 @@ def generate_launch_description():
     add_launch_arg("enable_blockage_diag", "true")
 
     add_launch_arg("calibration_file", "")
+    add_launch_arg("calibration_download_enabled")
     add_launch_arg("output_as_sensor_frame", "True", "output final pointcloud in sensor frame")
     add_launch_arg("use_dual_return_filter", "false")
     add_launch_arg("point_filters.downsample_mask.path", "")
