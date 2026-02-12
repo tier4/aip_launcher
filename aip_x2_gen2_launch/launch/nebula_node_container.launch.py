9a0e56e54a5a188c46826d4b33b7d50cfc44b7af
Koichi Imai <koichi.imai.2@tier4.jp>
2026-02-12 11:00:45 +0900

    use agnocast_component_container_cie for nebula_node_container
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- aip_x2_gen2_launch/launch/nebula_node_container.launch.py
+++ aip_x2_gen2_launch/launch/nebula_node_container.launch.py
@@ -525,7 +525,7 @@ def launch_setup(context, *args, **kwargs):
 
     container_exec = "agnocast_component_container" if use_agnocast else "component_container"
     container_exec_mt = (
-        "agnocast_component_container_mt" if use_agnocast else "component_container_mt"
+        "agnocast_component_container_cie" if use_agnocast else "component_container_mt"
     )
 
     set_container_executable = SetLaunchConfiguration(
